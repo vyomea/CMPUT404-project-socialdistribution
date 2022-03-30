f8c7e6e96386b76291f7945a4edeeae12d7c7f7e
Vyome Agarwal <vyome@ualberta.ca>
2022-03-30 20:32:32 +0000

    potential fix

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -40,4 +40,11 @@ if (process.env.NODE_ENV !== 'test') {
   });
 }
 
+app.get('/*', (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
+
 export default app;
