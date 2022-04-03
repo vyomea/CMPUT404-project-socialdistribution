8c8f50d9f8931a6aac5abac82d8cb6acf59dc5ea
Vyome Agarwal <vyome@ualberta.ca>
2022-04-03 02:03:16 +0000

    following is linked to profiles

--- src/app/src/components/FollowingList.tsx
+++ src/app/src/components/FollowingList.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 import api from "../api/api";
+import Author from "../api/models/Author";
 
 const FollowingList = ({ data }: any) => {
-  const [followersList, setFollowing] = useState<string[]>([]);
+  const [followersList, setFollowing] = useState<Author[]>([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const res = api.authors.withId("" + data?.id).followings.list();
         res.then((followings) => {
-            setFollowing(followings.map((following) => following.displayName));
+          setFollowing(followings);
         });
       } catch (err) {
         console.log(err);
@@ -18,7 +19,22 @@ const FollowingList = ({ data }: any) => {
     }
     fetchData();
   }, [data?.id]);
-  return <div>{followersList}</div>;
+  return (
+    <div>
+      {followersList.map((follower) => (
+        <a
+          href={
+            window.location.href.substring(
+              0,
+              window.location.href.search(data) - 1
+            ) + follower.id
+          }
+        >
+          {follower.displayName}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default FollowingList;
