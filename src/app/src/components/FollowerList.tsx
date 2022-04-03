331f6d706647bfa42b6f5fb070440bc60121d851
Vyome Agarwal <vyome@ualberta.ca>
2022-04-03 01:56:14 +0000

    display name linked to profile for follower

--- src/app/src/components/FollowerList.tsx
+++ src/app/src/components/FollowerList.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
-
 import api from "../api/api";
+import Author from "../api/models/Author";
+
 
 const FollowerList = ({ data }: any) => {
-  const [followersList, setFollowers] = useState<string[]>([]);
+  const [followersList, setFollowers] = useState<Author[]>([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const res = api.authors.withId("" + data?.id).followers.list();
         res.then((followers) => {
-          setFollowers(followers.map((follower) => follower.displayName));
+          setFollowers(followers);
         });
       } catch (err) {
         console.log(err);
@@ -18,7 +19,23 @@ const FollowerList = ({ data }: any) => {
     }
     fetchData();
   }, [data?.id]);
-  return <div>{followersList}</div>;
+
+  return (
+    <div>
+      {followersList.map((follower) => (
+        <a
+          href={
+            window.location.href.substring(
+              0,
+              window.location.href.search(data) - 1
+            ) + follower.id
+          }
+        >
+          {follower.displayName}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default FollowerList;
